ce159c4aea2839885eeccd7c13072e9d66f5309a
satyawan12 <sattukhatkar@gmail.com>
2025-12-07 17:40:42 +0530

    Fix DATABASES config for Render

--- kitchen_project/settings.py
+++ kitchen_project/settings.py
@@ -89,6 +89,12 @@ WSGI_APPLICATION = "kitchen_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 
