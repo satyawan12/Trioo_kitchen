2d43068452b133b4feab2ed08a3db87b9eb05e11
satyawan12 <sattukhatkar@gmail.com>
2026-03-09 23:05:09 +0530

    debug render error

--- kitchen_project/settings.py
+++ kitchen_project/settings.py
@@ -29,11 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 
-DEBUG=False
+DEBUG=True
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
